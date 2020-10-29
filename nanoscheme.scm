e5640a98dcdfbb6a04876f2ce110c6f95c73aa20
Oren Daniel <oren_daniel@protonmail.com>
2020-10-29 19:28:55 +0200

    rewrite in progress

--- nanoscheme.scm
+++ nanoscheme.scm
@@ -14,53 +14,40 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Environment 
+;; Environment 
+;;----------------------------------------------------------------------------------------------
+
+; every environment has an outer environment pointer
+; that represent the outer lexical scope
 (define make-environment (lambda (outer)
-	;; every environment has an outer environment pointer
-	;; that represent the outer lexical scope
-
-	(define cells-count 10) ; 10 cells for each hash table
-	(define dict (make-vector cells-count)) 
-	(vector-fill! dict '()) 
-
-	(define find-on-list (lambda (lst name fn op prms)
-		(cond
-			((null? lst) (if (null? outer) '() (apply (outer op) prms)))
-			((eqv? (caar lst) name) (fn lst))
-			(else (find-on-list (cdr lst) name fn op prms)))))
-
-	(define defined? (lambda (lst name)
-		(cond
-			((null? lst) #f)
-			((eqv? (caar lst) name) #t)
-			(else (defined? (cdr lst) name)))))
-
-	;; Hash function
-	(define hash (lambda (name)
-		(modulo (string-length (symbol->string name)) cells-count)))
+	
+	; for simplicity the environment will be represented as an assoc-list
+	(define dict '())
 
 	;; Definer
 	(define dict-define! (lambda (name value)
-		(if (not (defined? (vector-ref dict (hash name)) name))
-			(begin 
-				(vector-set! dict (hash name)
-					(cons (cons name value) (vector-ref dict (hash name))))
-				"defined")
-			"cannot redefine"))) ; dont allow to redefine in the same scope
+		(set! dict (cons (cons name value) dict)) "defined"))
 
 	;; Setter
 	(define dict-set! (lambda (name value)
-		(let ((cell (vector-ref dict (hash name))))
-			(if (eqv? (find-on-list cell   ; '() is evaluated as #t
-						name 
-						(lambda (lst) (set-cdr! (car lst) value))
-						'set (list name value)) '())
-				"cannot set undefined variable"
-				"setted"))))
+		(define helper (lambda (lst)
+			(cond
+				((null? lst) 
+					(if (null? outer) 
+						(begin (display "cannot set undefined entry\n") '())
+						((outer 'set) name value)))
+				((eqv? (caar lst) name) (set-cdr! (car lst) value) '())
+				(else (helper (cdr lst))))))
+		(helper dict)))
 
 	;; Getter
 	(define dict-get (lambda (name)
-		(find-on-list (vector-ref dict (hash name)) name cdar 'get (list name))))
+		(let ((res (assoc name dict)))
+			(if res
+				(cdr res)
+				(if (null? outer) 
+					(begin (display "cannot get undefined entry\n") '())
+					((outer 'get) name))))))
 
 	(lambda (cmd)
 		(cond
@@ -89,13 +76,14 @@
 
 (define unquoted? (lambda (expr) (eqv? (car expr) 'unquote)))
 
-(define unquotate (lambda (expr env) (_eval (_eval (cadr expr) env) env)))
+(define unquotate (lambda (expr env)
+	(nanoscheme-eval (nanoscheme-eval (cadr expr) env) env)))
 
 ;; Assignment expression
 ;;----------------------------------------------------------------------------------------------
 
 (define assignment? (lambda (expr) 
-	(and (eqv? (car expr) 'set) (symbol? (car expr)))))
+	(and (eqv? (car expr) 'set!) (symbol? (car expr)))))
 
 (define assignment-name (lambda (expr) (cadr expr)))
 
@@ -182,11 +170,11 @@
 (define sequence-eval (lambda (exprs env)
 	(if (not (null? (cdr exprs)))
 		(begin 
-			(_eval (car exprs) env)
+			(nanoscheme-eval (car exprs) env)
 			(sequence-eval (cdr exprs) env))
-		(_eval (car exprs) env))))
+		(nanoscheme-eval (car exprs) env))))
 
-(define _apply (lambda (operator operands)
+(define nanoscheme-apply (lambda (operator operands)
 	(cond
 		((procedure? operator) ;;primitive procedure NOT user defined procedure
 			(apply operator operands))
@@ -200,27 +188,26 @@
 
 		(else (error "Cannot apply a non procedure")))))
 			
-
 ;; Eval
 ;;----------------------------------------------------------------------------------------------
 
 (define eval-name (lambda (expr env)
 	(if (or (symbol? expr) (self? expr))
 		expr
-		(eval-name (_eval expr env) env))))
+		(eval-name (nanoscheme-eval expr env) env))))
 
 (define eval-assignment (lambda (expr env)
 	(let ((name (eval-name (assignment-name expr) env)))
-		((env 'set) name (_eval (assignment-value expr) env)))))
+		((env 'set) name (nanoscheme-eval (assignment-value expr) env)))))
 
 (define eval-definition (lambda (expr env)
 	(let ((name (eval-name (definition-name expr) env)))
-			((env 'def) name (_eval (definition-value expr) env)))))
+			((env 'def) name (nanoscheme-eval (definition-value expr) env)))))
 
 (define eval-if (lambda (expr env)
-	(if (_eval (if-predicate expr) env)
-		(_eval (if-consequent expr) env)
-		(_eval (if-alternative expr) env))))
+	(if (nanoscheme-eval (if-predicate expr) env)
+		(nanoscheme-eval (if-consequent expr) env)
+		(nanoscheme-eval (if-alternative expr) env))))
 
 (define eval-parameters (lambda (params env)
 	(cond
@@ -232,11 +219,11 @@
 (define eval-eager-operands (lambda (lst env)
 	(if (null? lst)
 		'()
-		(cons (_eval (car lst) env) (eval-eager-operands (cdr lst) env)))))
+		(cons (nanoscheme-eval (car lst) env) (eval-eager-operands (cdr lst) env)))))
 
 (define eval-literal-operands (lambda (lst env) lst))
 
-(define _eval (lambda (expr env)
+(define nanoscheme-eval (lambda (expr env)
 	(cond
 		((self? expr) expr)
 		((variable? expr) ((env 'get) expr))
@@ -254,180 +241,9 @@
 				(eval-parameters (parameters expr) env)
 				(body expr) env))
 		((call? expr)
-			(let ((prc (_eval (operator expr) env)))
-				(if (not (null? prc))
-					(if (function? prc)
-						(_apply prc (eval-eager-operands (operands expr) env))
-						(_apply prc (eval-literal-operands (operands expr) env)))
-					(begin (display "call of not a function\n") '()))))
+			(let ((prc (nanoscheme-eval (operator expr) env)))
+				(if (function? prc)
+					(nanoscheme-apply prc (eval-eager-operands (operands expr) env))
+					(nanoscheme-apply prc (eval-literal-operands (operands expr) env)))))
 		(else (error "unknown expression")))))
 
-
-;; Nanoscheme core
-;;--------------------------------------------------------------------------------------------------
-(define nanoscheme-core (lambda ()
-	(define env (make-environment '()))
-
-	((env 'def) 'eval _eval)
-	((env 'def) 'apply _apply)
-	((env 'def) 'empty-environment (lambda () (make-environment '())))
-
-	(lambda (cmd)
-		(cond 
-			((eqv? cmd 'environment) env)
-			((eqv? cmd 'eval)
-				(lambda (expr) (_eval expr env)))))))
-
-;; Lightweight Environment
-;;--------------------------------------------------------------------------------------------------
-(define nanoscheme (lambda ()
-	(define core (nanoscheme-core))
-
-	((core 'eval) 
-		'(define or (macro (... *or_STMTS*)
-			(define *or_HELPER* (lambda (*or_STMTS*)
-				(if (null? *or_STMTS*)
-					#f
-					(if ,(car *or_STMTS*)
-						#t
-						(*or_HELPER* (cdr *or_STMTS*)))))) 
-			(*or_HELPER* *or_STMTS*))))
-
-	((core 'eval) 
-		'(define and (macro (... *and_STMTS*)
-			(define *and_HELPER* (lambda (*and_STMTS*)
-				(if (null? *and_STMTS*)
-					#t
-					(if (not ,(car *and_STMTS*))
-						#f
-						(*and_HELPER* (cdr *and_STMTS*)))))) 
-			(*and_HELPER* *and_STMTS*))))
-	
-	(((core 'environment) 'def) 'car car)
-	(((core 'environment) 'def) 'cdr cdr)
-	(((core 'environment) 'def) 'cons cons)
-	(((core 'environment) 'def) 'set-car! set-car!)
-	(((core 'environment) 'def) 'set-cdr! set-cdr!)
-
-	(((core 'environment) 'def) '+ +)
-	(((core 'environment) 'def) '* *)
-	(((core 'environment) 'def) '- -)
-	(((core 'environment) 'def) '/ /)
-	(((core 'environment) 'def) 'expt expt)
-	(((core 'environment) 'def) 'modulo modulo)
-
-	(((core 'environment) 'def) 'not not)
-	(((core 'environment) 'def) '< <)
-	(((core 'environment) 'def) '<= <=)
-	(((core 'environment) 'def) '> >)
-	(((core 'environment) 'def) '>= >=)
-	(((core 'environment) 'def) '= =)
-	(((core 'environment) 'def) 'eq? eq?)
-	(((core 'environment) 'def) 'eqv? eqv?)
-	(((core 'environment) 'def) 'equal? equal?)
-
-	(((core 'environment) 'def) 'null? null?)
-	(((core 'environment) 'def) 'pair? pair?)
-	(((core 'environment) 'def) 'char? char?)
-	(((core 'environment) 'def) 'symbol? symbol?)
-	(((core 'environment) 'def) 'number? number?)
-	(((core 'environment) 'def) 'vector? vector?)
-	(((core 'environment) 'def) 'string? string?)
-	(((core 'environment) 'def) 'boolean? boolean?)
-
-	(((core 'environment) 'def) 'integer? integer?)
-	(((core 'environment) 'def) 'real? real?)
-	(((core 'environment) 'def) 'complex? complex?)
-
-	(((core 'environment) 'def) 'string->number string->number)
-	(((core 'environment) 'def) 'string->symbol string->symbol)
-	(((core 'environment) 'def) 'symbol->string symbol->string)
-	(((core 'environment) 'def) 'number->string number->string)
-
-	(((core 'environment) 'def)
-		'procedure? 
-		(lambda (prc) (or (procedure? prc) (eqv? (car prc) 'FNC) (eqv? (car prc) 'MCR))))
-
-	(((core 'environment) 'def) 'make-rectangular make-rectangular)
-	(((core 'environment) 'def) 'make-polar make-polar)
-	(((core 'environment) 'def) 'make-string make-string)
-	(((core 'environment) 'def) 'string-length string-length)
-	(((core 'environment) 'def) 'string-ref string-ref)
-	(((core 'environment) 'def) 'string-set! string-set!)
-	(((core 'environment) 'def) 'string-append string-append)
-	(((core 'environment) 'def) 'make-vector make-vector)
-	(((core 'environment) 'def) 'vector-length vector-length)
-	(((core 'environment) 'def) 'vector-ref vector-ref)
-	(((core 'environment) 'def) 'vector-set! vector-set!)
-
-	(((core 'environment) 'def) 'display (lambda (text) (display text) (newline) '()))
-	(((core 'environment) 'def) 'read read)
-	(((core 'environment) 'def) 'exit exit)
-	(((core 'environment) 'def) 'error error)
-
-	core))
-
-;; Full Scheme Environment
-;;--------------------------------------------------------------------------------------------------
-(define nanoscheme-full (lambda ()
-	(define core (nanoscheme))
-
-	((core 'eval)
-		'(define map (lambda (prc lst)
-			(if (null? lst)
-				'()
-				(cons (prc (car lst)) (map prc (cdr lst)))))))
-
-	((core 'eval) 
-		'(define begin (macro (... *begin_PRM*)
-			,(cons (append (list 'lambda '()) *begin_PRM*) '()))))
-
-	((core 'eval)
-		'(define cond (macro (... *cond_STMTS*)
-			(define else #t)
-			(define *cond_HELPER* (lambda (*cond_STMTS*)
-				(if (null? *cond_STMTS*)
-					'()
-					(if ,(caar *cond_STMTS*)
-						,(cons (append '(lambda ()) (cdar *cond_STMTS*)) '())
-						(*cond_HELPER* (cdr *cond_STMTS*))))))
-
-			(*cond_HELPER* *cond_STMTS*))))
-
-	((core 'eval)
-		'(define let (macro (*let_TUPLES* ... *let_REST*)
-			,(append 
-				(cons (append 
-					(append '(lambda) (cons 
-						(map car *let_TUPLES*)
-						'()))
-					*let_REST*) '())
-				(map cadr *let_TUPLES*)))))
-
-	(((core 'environment) 'def) 'string-fill! string-fill!)
-	(((core 'environment) 'def) 'vector-fill! vector-fill!)
-
-	(((core 'environment) 'def) 'list list)
-	(((core 'environment) 'def) 'list? list?)
-	(((core 'environment) 'def) 'list-ref list-ref)
-	(((core 'environment) 'def) 'reverse reverse)
-	(((core 'environment) 'def) 'append append)
-	(((core 'environment) 'def) 'member member)
-
-	(((core 'environment) 'def) 'list->vector list->vector)
-	(((core 'environment) 'def) 'vector->list vector->list)
-
-	(((core 'environment) 'def) 'caar caar)
-	(((core 'environment) 'def) 'cadr cadr)
-	(((core 'environment) 'def) 'cdar cdar)
-	(((core 'environment) 'def) 'cddr cddr)
-	(((core 'environment) 'def) 'caaar caaar)
-	(((core 'environment) 'def) 'caadr caadr)
-	(((core 'environment) 'def) 'cadar cadar)
-	(((core 'environment) 'def) 'caddr caddr)
-	(((core 'environment) 'def) 'cdaar cdaar)
-	(((core 'environment) 'def) 'cdadr cdadr)
-	(((core 'environment) 'def) 'cddar cddar)
-	(((core 'environment) 'def) 'cdddr cdddr)
-
-	core))
--- repl.scm
+++ repl.scm
@@ -1,12 +1,173 @@
-;; Example for a simple repl
+;; Nanoscheme
+;; Copyright (C) 2020  Oren Daniel
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (load "nanoscheme.scm")
 
-(define nscm (nanoscheme-full))
+(define env (make-environment '()))
+
+((env 'def) 'eval nanoscheme-eval)
+((env 'def) 'apply nanoscheme-apply)
+((env 'def) 'empty-environment (lambda () (make-environment '())))
+
+
+((env 'def) 'car car)
+((env 'def) 'cdr cdr)
+((env 'def) 'cons cons)
+((env 'def) 'set-car! set-car!)
+((env 'def) 'set-cdr! set-cdr!)
+
+((env 'def) '+ +)
+((env 'def) '* *)
+((env 'def) '- -)
+((env 'def) '/ /)
+((env 'def) 'expt expt)
+((env 'def) 'modulo modulo)
+
+((env 'def) 'not not)
+((env 'def) '< <)
+((env 'def) '<= <=)
+((env 'def) '> >)
+((env 'def) '>= >=)
+((env 'def) '= =)
+((env 'def) 'eq? eq?)
+((env 'def) 'eqv? eqv?)
+((env 'def) 'equal? equal?)
+
+((env 'def) 'null? null?)
+((env 'def) 'pair? pair?)
+((env 'def) 'char? char?)
+((env 'def) 'symbol? symbol?)
+((env 'def) 'number? number?)
+((env 'def) 'vector? vector?)
+((env 'def) 'string? string?)
+((env 'def) 'boolean? boolean?)
+
+((env 'def) 'integer? integer?)
+((env 'def) 'real? real?)
+((env 'def) 'complex? complex?)
+
+((env 'def) 'string->number string->number)
+((env 'def) 'string->symbol string->symbol)
+((env 'def) 'symbol->string symbol->string)
+((env 'def) 'number->string number->string)
+
+((env 'def)
+	'procedure? 
+	(lambda (prc) (or (procedure? prc) (eqv? (car prc) 'FNC) (eqv? (car prc) 'MCR))))
+
+((env 'def) 'make-rectangular make-rectangular)
+((env 'def) 'make-polar make-polar)
+((env 'def) 'make-string make-string)
+((env 'def) 'string-length string-length)
+((env 'def) 'string-ref string-ref)
+((env 'def) 'string-set! string-set!)
+((env 'def) 'string-append string-append)
+((env 'def) 'make-vector make-vector)
+((env 'def) 'vector-length vector-length)
+((env 'def) 'vector-ref vector-ref)
+((env 'def) 'vector-set! vector-set!)
+
+((env 'def) 'display (lambda (text) (display text) (newline) '()))
+((env 'def) 'read read)
+((env 'def) 'exit exit)
+((env 'def) 'error error)
+
+((env 'def) 'assoc assoc)
+((env 'def) 'string-fill! string-fill!)
+((env 'def) 'vector-fill! vector-fill!)
+
+((env 'def) 'list list)
+((env 'def) 'list? list?)
+((env 'def) 'list-ref list-ref)
+((env 'def) 'reverse reverse)
+((env 'def) 'append append)
+((env 'def) 'member member)
+
+((env 'def) 'list->vector list->vector)
+((env 'def) 'vector->list vector->list)
+
+((env 'def) 'caar caar)
+((env 'def) 'cadr cadr)
+((env 'def) 'cdar cdar)
+((env 'def) 'cddr cddr)
+((env 'def) 'caaar caaar)
+((env 'def) 'caadr caadr)
+((env 'def) 'cadar cadar)
+((env 'def) 'caddr caddr)
+((env 'def) 'cdaar cdaar)
+((env 'def) 'cdadr cdadr)
+((env 'def) 'cddar cddar)
+((env 'def) 'cdddr cdddr)
+
+(nanoscheme-eval
+	'(define or (macro (... *or_STMTS*)
+		(define *or_HELPER* (lambda (*or_STMTS*)
+			(if (null? *or_STMTS*)
+				#f
+				(if ,(car *or_STMTS*)
+					#t
+					(*or_HELPER* (cdr *or_STMTS*)))))) 
+		(*or_HELPER* *or_STMTS*))) env)
+
+(nanoscheme-eval 
+	'(define and (macro (... *and_STMTS*)
+		(define *and_HELPER* (lambda (*and_STMTS*)
+			(if (null? *and_STMTS*)
+				#t
+				(if (not ,(car *and_STMTS*))
+					#f
+					(*and_HELPER* (cdr *and_STMTS*)))))) 
+		(*and_HELPER* *and_STMTS*))) env)
+
+
+(nanoscheme-eval
+	'(define map (lambda (prc lst)
+		(if (null? lst)
+			'()
+			(cons (prc (car lst)) (map prc (cdr lst)))))) env)
+
+(nanoscheme-eval 
+	'(define begin (macro (... *begin_PRM*)
+		,(cons (append (list 'lambda '()) *begin_PRM*) '()))) env)
+
+(nanoscheme-eval
+	'(define cond (macro (... *cond_STMTS*)
+		(define else #t)
+		(define *cond_HELPER* (lambda (*cond_STMTS*)
+			(if (null? *cond_STMTS*)
+				'()
+				(if ,(caar *cond_STMTS*)
+					,(cons (append '(lambda ()) (cdar *cond_STMTS*)) '())
+					(*cond_HELPER* (cdr *cond_STMTS*))))))
+
+		(*cond_HELPER* *cond_STMTS*))) env)
+
+(nanoscheme-eval
+	'(define let (macro (*let_TUPLES* ... *let_REST*)
+		,(append 
+			(cons (append 
+				(append '(lambda) (cons 
+					(map car *let_TUPLES*)
+					'()))
+				*let_REST*) '())
+			(map cadr *let_TUPLES*)))) env)
 
 (define (repl)
-	(display ">>")
-	(display ((nscm 'eval) (read)))
+	(display ">> ")
+	(display (nanoscheme-eval (read) env))
 	(newline)
 	(repl))
 
